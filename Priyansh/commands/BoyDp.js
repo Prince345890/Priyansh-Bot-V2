48fd3a003893e77cbacf20c1790cda0fd5df022f
Prince345890 <kr6962896@gmail.com>
2025-08-15 11:04:07 +0530

    Update BoyDp.js

--- Priyansh/commands/BoyDp.js
+++ Priyansh/commands/BoyDp.js
@@ -1,103 +1,95 @@
-const fs = global.nodemodule["fs-extra"];
-
 module.exports.config = {
   name: "boydp",
-  version: "1.0.0",
+  version: "1.0.6",
   hasPermssion: 0,
-  credits: "PREM BABU",
-  description: "Boys Dp photos",
+  credits: "PREM BABU (Edited by Aryan + Fixed by ChatGPT)",
+  description: "Send random boy DP photo with yes/no confirm (full list, stable upload & retry)",
   commandCategory: "Random-IMG",
   usages: "boydp",
   cooldowns: 2,
   dependencies: {
     "request": "",
-    "fs-extra": "",
-    "axios": ""
+    "fs-extra": ""
   }
 };
 
 module.exports.run = async ({ api, event }) => {
+  const prompt = "Abe Yaar Pura Command To Likh Le 😹 ckbot Yes Or Not ? (reply: yes / no)";
+  const commandName = module.exports.config.name;
+
+  api.sendMessage(prompt, event.threadID, (err, info) => {
+    if (err) return;
+    global.client.handleReply.push({
+      name: commandName,
+      messageID: info.messageID,
+      author: event.senderID,
+      type: "confirm"
+    });
+  }, event.messageID);
+};
+
+module.exports.handleReply = async ({ api, event, handleReply }) => {
+  if (handleReply.type !== "confirm" || event.senderID !== handleReply.author) return;
+
+  const fs = global.nodemodule["fs-extra"];
   const request = global.nodemodule["request"];
+  const { join } = require("path");
+
+  const msg = (event.body || "").trim().toLowerCase();
+  if (msg !== "yes" && msg !== "no") return;
+
+  if (msg === "no") {
+    return api.sendMessage("Thik hai, cancel kar diya 😏", event.threadID, () => {}, handleReply.messageID);
+  }
+
+  // YES → send random DP
   const links = [
-    "https://i.imgur.com/lGowut2.jpg",
-    "https://i.imgur.com/4qDvuWi.jpg",
-    "https://i.imgur.com/vu5yPzj.jpg",
-    "https://i.imgur.com/ZCYaMfF.jpeg",
-    "https://i.imgur.com/hSQWcAM.jpeg",
-    "https://i.imgur.com/ovX6lfA.jpeg",
-    "https://i.imgur.com/RgfrYpM.jpeg",
-    "https://i.imgur.com/DfvFLov.jpeg",
-    "https://i.imgur.com/GPwbrDj.jpeg",
-    "https://i.imgur.com/jSifYwo.jpeg",
-    "https://i.imgur.com/Chc5pLl.jpeg",
-    "https://i.imgur.com/HbznJXK.jpeg",
-    "https://i.imgur.com/OLKdt61.jpeg",
-    "https://i.imgur.com/tDNqmML.jpeg",
-    "https://i.imgur.com/yUwx4o4.jpeg",
-    "https://i.imgur.com/e4xWHUv.jpeg",
-    "https://i.imgur.com/q6LfLx0.jpeg",
-    "https://i.imgur.com/eoKKdzI.jpeg",
-    "https://i.imgur.com/n3DS2ha.jpeg",
-    "https://i.imgur.com/E5QWGCE.jpeg",
-    "https://i.imgur.com/44YNGf6.jpeg",
-    "https://i.imgur.com/fh8i2Ph.jpeg",
-    "https://i.imgur.com/EMazlEj.jpeg",
-    "https://i.imgur.com/Uz4RQSg.jpeg",
-    "https://i.imgur.com/INxT1BF.jpeg",
-    "https://i.imgur.com/jnU2FrO.jpeg",
-    "https://i.imgur.com/qFDKN6v.jpeg",
-    "https://i.imgur.com/m84lelb.jpeg",
-    "https://i.imgur.com/FmMsaOR.jpeg",
-    "https://i.imgur.com/Ln7It9C.jpeg",
-    "https://i.imgur.com/SZ9KznS.jpeg",
-    "https://i.imgur.com/WypMeee.jpeg",
-    "https://i.imgur.com/Zq9sgX0.jpeg",
-    "https://i.imgur.com/kIvSt9A.jpeg",
-    "https://i.imgur.com/g3R1fQh.jpeg",
-    "https://i.imgur.com/jv1LGtq.jpeg",
-    "https://i.imgur.com/lKkm83o.jpeg",
-    "https://i.imgur.com/Yuai95W.jpeg",
-    "https://i.imgur.com/FNWIrNo.jpeg",
-    "https://i.imgur.com/YUOScB2.jpeg",
-    "https://i.imgur.com/Gd8K8Cg.jpeg",
-    "https://i.imgur.com/R0mvOeZ.jpeg",
-    "https://i.imgur.com/GGLiv35.jpeg",
-    "https://i.imgur.com/b4hHhSk.jpeg",
-    "https://i.imgur.com/45QWr06.jpeg",
-    "https://i.imgur.com/uz7bh1h.jpeg",
-    "https://i.imgur.com/7blSNAk.jpeg",
-    "https://i.imgur.com/r11VKsm.jpeg",
-    "https://i.imgur.com/4NyGJmu.jpeg",
-    "https://i.imgur.com/HMMe7fV.jpeg",
-    "https://i.imgur.com/447Dsfb.jpeg",
-    "https://i.imgur.com/BsfPGOF.jpeg",
-    "https://i.imgur.com/h0C5puK.jpeg",
-    "https://i.imgur.com/qpgBE0X.jpeg",
-    "https://i.imgur.com/f0HFaCv.jpeg",
-    "https://i.imgur.com/a4vo9Cv.jpeg",
-    "https://i.imgur.com/J7PAAuR.jpeg",
-    "https://i.imgur.com/OG7CCAz.jpeg",
-    "https://i.imgur.com/tqnzYDJ.jpeg",
-    "https://i.imgur.com/3ItPOnW.jpeg",
-    "https://i.imgur.com/yCkue9w.jpeg",
-    "https://i.imgur.com/jx6VfM6.jpeg",
-    "https://i.imgur.com/52cEmKs.jpg",
-    "https://i.imgur.com/9xLfitZ.jpg",
-    "https://i.imgur.com/RJ3Lou6.jpg",
-    "https://i.imgur.com/dwAKjDy.jpg",
-    "https://i.imgur.com/qBlbbCX.jpg",
-    "https://i.imgur.com/lGowut2.jpg"
+    "https://i.imgur.com/h9kE3Ic.jpeg","https://i.imgur.com/nNJr9Dt.jpeg","https://i.imgur.com/wFSzYnF.jpeg",
+    "https://i.imgur.com/zJ1HFs8.jpeg","https://i.imgur.com/wsv4mWY.jpeg","https://i.imgur.com/r3UTDwz.jpeg",
+    "https://i.imgur.com/ZCYaMfF.jpeg","https://i.imgur.com/hSQWcAM.jpeg","https://i.imgur.com/ovX6lfA.jpeg",
+    "https://i.imgur.com/RgfrYpM.jpeg","https://i.imgur.com/DfvFLov.jpeg","https://i.imgur.com/GPwbrDj.jpeg",
+    "https://i.imgur.com/jSifYwo.jpeg","https://i.imgur.com/Chc5pLl.jpeg","https://i.imgur.com/HbznJXK.jpeg",
+    "https://i.imgur.com/OLKdt61.jpeg","https://i.imgur.com/tDNqmML.jpeg","https://i.imgur.com/yUwx4o4.jpeg",
+    "https://i.imgur.com/e4xWHUv.jpeg","https://i.imgur.com/q6LfLx0.jpeg","https://i.imgur.com/eoKKdzI.jpeg",
+    "https://i.imgur.com/n3DS2ha.jpeg","https://i.imgur.com/E5QWGCE.jpeg","https://i.imgur.com/44YNGf6.jpeg",
+    "https://i.imgur.com/fh8i2Ph.jpeg","https://i.imgur.com/EMazlEj.jpeg","https://i.imgur.com/Uz4RQSg.jpeg",
+    "https://i.imgur.com/INxT1BF.jpeg","https://i.imgur.com/jnU2FrO.jpeg","https://i.imgur.com/qFDKN6v.jpeg",
+    "https://i.imgur.com/m84lelb.jpeg","https://i.imgur.com/FmMsaOR.jpeg","https://i.imgur.com/Ln7It9C.jpeg",
+    "https://i.imgur.com/SZ9KznS.jpeg","https://i.imgur.com/WypMeee.jpeg","https://i.imgur.com/Zq9sgX0.jpeg",
+    "https://i.imgur.com/kIvSt9A.jpeg","https://i.imgur.com/g3R1fQh.jpeg","https://i.imgur.com/jv1LGtq.jpeg",
+    "https://i.imgur.com/lKkm83o.jpeg","https://i.imgur.com/Yuai95W.jpeg","https://i.imgur.com/FNWIrNo.jpeg",
+    "https://i.imgur.com/YUOScB2.jpeg","https://i.imgur.com/Gd8K8Cg.jpeg","https://i.imgur.com/R0mvOeZ.jpeg",
+    "https://i.imgur.com/GGLiv35.jpeg","https://i.imgur.com/b4hHhSk.jpeg","https://i.imgur.com/45QWr06.jpeg",
+    "https://i.imgur.com/uz7bh1h.jpeg","https://i.imgur.com/7blSNAk.jpeg","https://i.imgur.com/r11VKsm.jpeg",
+    "https://i.imgur.com/4NyGJmu.jpeg","https://i.imgur.com/HMMe7fV.jpeg","https://i.imgur.com/447Dsfb.jpeg",
+    "https://i.imgur.com/BsfPGOF.jpeg","https://i.imgur.com/h0C5puK.jpeg","https://i.imgur.com/qpgBE0X.jpeg",
+    "https://i.imgur.com/f0HFaCv.jpeg","https://i.imgur.com/a4vo9Cv.jpeg","https://i.imgur.com/J7PAAuR.jpeg",
+    "https://i.imgur.com/OG7CCAz.jpeg","https://i.imgur.com/tqnzYDJ.jpeg","https://i.imgur.com/3ItPOnW.jpeg",
+    "https://i.imgur.com/yCkue9w.jpeg","https://i.imgur.com/jx6VfM6.jpeg","https://i.imgur.com/52cEmKs.jpg",
+    "https://i.imgur.com/9xLfitZ.jpg","https://i.imgur.com/RJ3Lou6.jpg","https://i.imgur.com/dwAKjDy.jpg",
+    "https://i.imgur.com/qBlbbCX.jpg"
   ];
 
-  const path = __dirname + "/cache/1.jpg";
+  const cacheDir = join(__dirname, "cache");
+  fs.ensureDirSync(cacheDir);
+  const filePath = join(cacheDir, `boydp_${Date.now()}.jpg`);
   const randomLink = links[Math.floor(Math.random() * links.length)];
 
-  return request(encodeURI(randomLink))
-    .pipe(fs.createWriteStream(path))
-    .on("close", () => {
-      api.sendMessage({
-        body: "💝 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐀𝐚𝐫𝐲𝐚𝐧 𝐁𝐚𝐛𝐮",
-        attachment: fs.createReadStream(path)
-      }, event.threadID, () => fs.unlinkSync(path));
-    });
+  const sendAttachment = () => {
+    request(encodeURI(randomLink))
+      .pipe(fs.createWriteStream(filePath))
+      .on("close", () => {
+        api.sendMessage({
+          body: "💝 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐀𝐚𝐫𝐲𝐚𝐧 𝐁𝐚𝐛𝐮",
+          attachment: fs.createReadStream(filePath)
+        }, event.threadID, (err) => {
+          if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+          if (err) {
+            api.sendMessage(`Kuch gadbad ho gayi 😅. Yeh lo link:\n${randomLink}`, event.threadID, handleReply.messageID);
+          }
+        }, handleReply.messageID);
+      });
+  };
+
+  sendAttachment();
 };
